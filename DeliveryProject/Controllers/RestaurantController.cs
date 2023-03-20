459e7748ea8be861ee0cf1c375e97a2b06858796
andreiggar <andreiggar@hotmail.com>
2023-03-21 00:20:14 +0100

    More updates

---
+++ DeliveryProject/Controllers/RestaurantController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryProject.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+
+            return View();
+        }
+    }
+}
